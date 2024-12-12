715fca9fde0ac6cc24b7731b302dd9382ada0666
SadhanaKarki <sadhanakarki141@gmail.com>
2024-12-12 21:22:13 +0545

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@
 import 'package:e_commerce_app/Pages/front_page.dart';
 import 'package:e_commerce_app/wishlist/bloc/wishlist_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_messaging/firebase_messaging.dart';
+//import 'package:flutter/material.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'firebase_options.dart';
+//import 'firebase_options.dart';
 
 void main() {
   runApp(
